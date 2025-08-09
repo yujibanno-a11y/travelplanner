09dc8085ca9c754847c13bd3050a999ddfc53cb4
psl31 <leela2112@gmail.com>
2025-08-09 16:39:09 -0400

    Updated SignupPage.tsx

--- src/components/SignupPage.tsx
+++ src/components/SignupPage.tsx
@@ -70,11 +70,9 @@ const SignupPage: React.FC<SignupPageProps> = ({ onBack, onSignup, onNavigateToL
 
   const handleInputChange = (field: string, value: string) => {
     setFormData(prev => ({ ...prev, [field]: value }));
-    // Clear specific field error when user starts typing
-    setErrors(prev => ({ ...prev, [field]: '' }));
-    // Clear auth error when user makes changes
-    if (authError) {
-      setAuthError('');
+    // Clear error when user starts typing
+    if (errors[field]) {
+      setErrors(prev => ({ ...prev, [field]: '' }));
     }
   };
 
